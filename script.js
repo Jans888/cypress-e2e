b96815d6af34f4df7a0c10f35f87f46552d22147
Jans888 <j.sumanas@gmail.com>
2021-09-22 13:09:05 +0300

    Script was added

--- script.js
+++ script.js
@@ -2,23 +2,25 @@ const fs = require('fs')
 
 const fileContents = fs.readFileSync('log').toString()
 let details = []
-let keyStringStart = '.  AssertionError  '
+let keyStringStart = 'AssertionError:'
 let section = fileContents.split(keyStringStart)
+console.log(section.length);
 
 if (section.length > 1) {
     for (var i = 1; i < section.length; i++) {
-        let string = section[i].substring( 0, section[i].indexOf( ", time differense in seconds:" ) ).replace("", "")
-        details.push(string)
+        let errorTemp = section[1].substring( 0, section[1].indexOf( "ts:" ) ).replace("      at Context.eval (https://example.cypress.io/__cypress/tests?p=", "-> ")
+        let addText = "AssertionError:"
+        let error = addText.concat(errorTemp).concat("ts")
+        details.push(error)
     }
 } else {
     details = ["No issues"]
 }
 
 details = details.join('\n')
-console.log(details);
 
 fs.writeFile("details.txt", details, function (err) {
 
     // Checks if there is an error
     if (err) return console.log(err);
-  });
+  })
